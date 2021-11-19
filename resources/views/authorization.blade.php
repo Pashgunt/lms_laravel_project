9e317812a200649afcdde5ed7fc4823995b7e856
Pavel <antonov.pavel.00@yandex.ru>
2021-11-19 12:41:42 +0300

    style

--- resources/views/authorization.blade.php
+++ resources/views/authorization.blade.php
@@ -11,7 +11,7 @@
                 @csrf
                 <div class="registration__title _title">Авторизация</div>
                 <label class="registration__label _title">Логин <br><input name="login" type="text"></label>
-                <label class="registration__label _title"> Пароль <a href="/recovery">Забыли пароль?</a><br>
+                <label class="registration__label _title"> Пароль <a href="/recovery" class = "registration__recovery">Забыли пароль?</a><br>
                     <input name="password" type="password">
                 </label>
                 <input type="submit" value="Авторизоваться" name="authUser">
--- resources/views/recovery.blade.php
+++ resources/views/recovery.blade.php
@@ -9,8 +9,8 @@
         <section class="registration__wrapper _container">
             <form action="/" method="post">
                 @csrf
-                <span class="registration__title _title">Восстановление пароля</span>
-                <label class="_title">Почта <br><input name="email" type="email"></label>
+                <div class="registration__title _title">Восстановление пароля</div>
+                <label class="registration__label _title">Почта <br><input name="email" type="email"></label>
                 <input type="submit" value="Отправить новый пароль" name="sendPassword">
             </form>
         </section>
