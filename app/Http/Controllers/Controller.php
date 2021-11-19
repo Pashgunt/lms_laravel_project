12572699e9a2d3efa4e69d512ba130be84e40334
Pavel <antonov.pavel.00@yandex.ru>
2021-11-19 16:07:16 +0300

    валидация

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Http\Controllers\ValidateController as Validator;
+use App\Http\Request\ValidateController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected ValidateController $validateController;
+
+    function __construct()
+    {
+        $this->validateController = new ValidateController();
+    }
+
 }
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -10,10 +10,26 @@
             <form action="/register" method="post">
                 @csrf
                 <div class="registration__title _title">Регистрация</div>
-                <label class="registration__label _title">Имя пользователя <br><input name="username" type="text"></label>
-                <label class="registration__label _title">Почта <br><input name="email" type="email"></label>
-                <label class="registration__label _title">Пароль <br><input name="password" type="password"></label>
-                <label class="registration__label _title">Подтверждение пароля <br><input name="rePassword" type="password"></label>
+                <label class="registration__label _title">Имя пользователя
+                    @error('username')
+                    <div class="alert-danger">{{ $message }}</div>
+                    @enderror
+                    <br><input name="username" type="text"></label>
+                <label class="registration__label _title">Почта
+                    @error('email')
+                    <div class="alert-danger">{{ $message }}</div>
+                    @enderror
+                    <br><input name="email" type="email"></label>
+                <label class="registration__label _title">Пароль
+                    @error('password')
+                    <div class="alert-danger">{{ $message }}</div>
+                    @enderror
+                    <br><input name="password" type="password"></label>
+                <label class="registration__label _title">Подтверждение пароля
+                    @error('rePassword')
+                    <div class="alert-danger">{{ $message }}</div>
+                    @enderror
+                    <br><input name="rePassword" type="password"></label>
                 <input type="submit" value="Зарегестрироваться" name="register">
             </form>
         </section>
