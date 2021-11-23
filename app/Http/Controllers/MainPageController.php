82a12a62443fd1881944d5644bfd7b09e03202d7
nasty sapr <anastasia.i.saprykina@gmail.com>
2021-11-23 12:40:23 +0300

    исправление ошибок, связанных с авторизацией

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class MainPageController
 {
-    /** Метод отображение базовой страницы */
+    /** Метод отображение базовой страницы   */
     public function main ()
     {
         return view('layout');
