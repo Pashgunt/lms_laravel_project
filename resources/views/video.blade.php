fcc7b2814892a53306888aa237e607a4b21a72fc
AleksandrTm <aleksandr.uteam@gmail.com>
2021-11-24 14:37:15 +0300

    Вывод плеера

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -7,22 +7,8 @@
 
 @section ('content')
     <section>
-        <video
-            id="my-video"
-            class="video-js"
-            controls
-            preload="auto"
-            width="1024"
-            height="768"
-            data-setup="{}">
-
-            <source src="assets/video/test_video.mp4" type="video/mp4"/>
-            <p class="vjs-no-js">
-                To view this video please enable JavaScript, and consider upgrading to a web browser that
-                <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-            </p>
+        <video id="demo_video" class="video-js" controls width="1024" height="768">
+            <source src="assets/video/test_video.mp4" type="video/mp4">
         </video>
-
-        <script src="https://vjs.zencdn.net/7.17.0/video.min.js"></script>
     </section>
 @endsection
