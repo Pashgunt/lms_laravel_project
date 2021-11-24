fbcf3fc904cfb513ca2d6b510aa45a6540f1c8fb
nasty sapr <anastasia.i.saprykina@gmail.com>
2021-11-24 10:50:51 +0300

    вывод информации о курсе

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ValidateRequest\RegRequest;
 use App\LMS\Repositories\UserRepository;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 class RegisteredUserController extends Controller
 {
--- resources/views/coursesList.blade.php
+++ resources/views/coursesList.blade.php
@@ -6,14 +6,19 @@
 
 @section('content')
 <h4>Список курсов</h4>
+
+<a href="/courses/create" class="btn btn-primary">
+    Создать новый курс
+</a>
+
 <div class="usersTable">
     <table class="table table-striped table-modify">
         <tr>
-            <td>Наименование</td>
-            <td>Автор</td>
-            <td>Описание</td>
-            <td></td>
-            <td></td>
+            <th>Наименование</th>
+            <th>Автор</th>
+            <th>Описание</th>
+            <th></th>
+            <th></th>
         </tr>
         @if (isset($coursesList))
             @foreach($coursesList as $course)
@@ -22,14 +27,14 @@
                     <td>{{$course->author}}</td>
                     <td>{{$course->description}}</td>
                     <td>
-                        <a href="/users/edit/{{$course->id}}" class="btn btn-primary">
+                        <a href="/courses/{{$course->id}}" class="btn btn-primary">
                             Информация
                         </a>
                     </td>
                     <td>
-                        <form action="" method="post">
+                        <form action="" method="get">
                             @csrf
-                            <input type="hidden" name="userId" value="{{$course->id}}">
+{{--                            <input type="hidden" name="detail" value="{{$course->id}}">--}}
                             <input type="submit" name="deleteUser" value="Удалить" class="btn btn-danger">
                         </form>
                     </td>
