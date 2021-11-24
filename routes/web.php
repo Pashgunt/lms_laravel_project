78f942951a7e0b93ab4b913bf0b669fe654b80b3
vova95nk <vova95nk@gmail.com>
2021-11-24 10:33:15 +0300

    Новая ветка для списка вложенных элементов курса.

--- routes/web.php
+++ routes/web.php
@@ -24,25 +24,47 @@ use \App\Http\Controllers\CourseController;
 Route::get('/', [MainPageController::class, 'main'])->middleware(['auth'])->name('index');
 
 Route::resource('/courses', CourseController::class)->missing(function (Request $request) {
-    return Redirect::route('courses.index');})->middleware(['auth', 'role:admin|manager']);
+    return Redirect::route('courses.index');
+})->middleware(['auth', 'role:admin|manager']);
 
-Route::get('/register', [RegisteredUserController::class, 'create'])->middleware('guest')->name('register');
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 
-Route::get('/users/list/{page}', '\App\Http\Controllers\UsersListController@main');
-Route::post('/users/list/{page}', '\App\Http\Controllers\UsersListController@delete');
-Route::get('/users/edit/{userId}', '\App\Http\Controllers\UsersListController@editPage');
-Route::post('/users/edit/{userId}', '\App\Http\Controllers\UsersListController@editInfo');
+Route::get('/users/list/{page}', [\App\Http\Controllers\UsersListController::class, 'main']);
 
-Route::get('/recovery', [PasswordResetLinkController::class, 'create'])->middleware('guest')->name('password.request');
+Route::post('/users/list/{page}', [\App\Http\Controllers\UsersListController::class, 'delete']);
 
-Route::post('/recovery', [PasswordResetLinkController::class, 'store'])->middleware('guest')->name('password.email');
+Route::get('/users/edit/{userId}', [\App\Http\Controllers\UsersListController::class, 'editPage']);
 
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->middleware('guest')->name('password.reset');
+Route::post('/users/edit/{userId}', [\App\Http\Controllers\UsersListController::class, 'editInfo']);
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])->middleware('guest')->name('password.update');
+Route::get('/recovery', [PasswordResetLinkController::class, 'create'])
+    ->middleware('guest')
+    ->name('password.request');
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])->middleware('guest')->name('login');
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
-Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+Route::post('/recovery', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+    ->middleware('guest')
+    ->name('password.reset');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.update');
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
+
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
 
