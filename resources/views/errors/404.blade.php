368af1bb0361c1d687c5927773896be1b4c0e972
nasty sapr <anastasia.i.saprykina@gmail.com>
2021-11-26 13:51:15 +0300

    обработка 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends("layout")
+@section('style')
+    <link rel="stylesheet" href="/assets/css/usersList.css">
+@endsection
+
+@section('content')
+    <section class="registration__wrapper _container">
+        <a href="/"><img src="/img/404.png" alt="" width="100%"></a>
+    </section>
+@endsection
