018d2d3d1bce4feac857d710d40bce85f823b6d8
nasty sapr <anastasia.i.saprykina@gmail.com>
2021-11-26 10:56:19 +0300

    удаление уникальных полей для таблицы courses

---
+++ database/migrations/2021_11_26_103423_delete_unique_name_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteUniqueNameCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('name')->change();
+            $table->dropUnique('courses_name_unique');
+            $table->dropUnique('courses_deleted_at_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('name')->unique()->change();
+        });
+    }
+}
