8dd23211262e50908282f75d9b0ac168c1ddd851
vova95nk <vova95nk@gmail.com>
2021-12-01 12:28:55 +0300

    First commit

--- resources/views/forms/activities/addTestActivity.blade.php
+++ resources/views/forms/activities/addTestActivity.blade.php
@@ -1,3 +1,23 @@
 <div class="test_activity">
-    <h5>Форма добавления теста</h5>
+    <label>
+        Заголовок -
+        <input type="text" name="title" placeholder="Заголовок" style="margin-bottom: 20px">
+    </label><br>
+    <label>
+        Описание - <br>
+        <textarea name="content" rows="5" cols="50"></textarea>
+    </label><br>
+    <label>
+        Количество вопросов -
+        <input type="number" min="1" max="12" class="count_questions" value="1">
+        <button type="button" class="add_question btn btn-success">Добавить</button>
+        <div class="questions_list">
+            <form action="" method="post">
+            Вопросы:
+            <div class="added_question">
+
+            </div>
+            </form>
+        </div>
+    </label>
 </div>
